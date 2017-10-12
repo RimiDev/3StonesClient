13dc8654eb324a919bbea9817a8f2df67274c692
Jephthia <jephthia4@hotmail.com>
2017-10-12 18:25:55 -0400

    The user can now place stones and visually see the board

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import io.bretty.console.table.Alignment;
+import io.bretty.console.table.ColumnFormatter;
+import io.bretty.console.table.Table;
 
 public class Board
 {
+	private List<String[]> columns = new ArrayList<String[]>();
+	
+	public Board()
+	{
+		resetBoard();
+	}
+	
+	public void resetBoard()
+	{
+		for(int i = 0; i < 12; i++)
+			columns.add(new String[11]);
+		
+		for(int col = 0; col < columns.size(); col++)
+		{
+			String[] column = columns.get(col);
+					
+			for(int row = 0; row < column.length; row++)
+			{
+				placeStone(col, row, "");
+			}
+		}
+	}
+	
+	public void placeStone(int x, int y, String tmpPlayerType)
+	{
+		columns.get(x)[y] = tmpPlayerType;
+	}
+	
+	public void draw()
+	{
+		String[] yIndexes = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
+		
+		ColumnFormatter<String> ageFormatter = ColumnFormatter.text(Alignment.CENTER, 3);
 
+		Table.Builder builder = new Table.Builder("", yIndexes, ageFormatter);
+		
+		for(int i = 1; i <= 11; i++)
+			builder.addColumn(""+i, columns.get(i), ageFormatter);
+
+		Table table = builder.build();
+		System.out.println(table);
+	}
+	
+	public boolean isEmpty(int x, int y)
+	{
+		//TODO
+		return false;
+	}
 }
--- src/MainApp.java
+++ src/MainApp.java
@@ -29,64 +29,35 @@ public class MainApp {
 }
 class TmpDrawingClass
 {
-	private List<String[]> columns = new ArrayList<String[]>();
 	private Scanner keyboard = new Scanner(System.in);
 	
 	public TmpDrawingClass()
 	{
+		Board board = new Board();
+		
 		String startORquit = promptString("Type start to start the game and quit to quit the game", new String[]{"start", "quit"}, "Try again and please enter start or quit");
 		
 		if(startORquit.equalsIgnoreCase("start"))
 		{
 			System.out.println("...STARTING GAME...");
 			
-			for(int i = 0; i < 12; i++)
-				columns.add(new String[11]);
+			board.draw();
 			
-			for(int col = 0; col < columns.size(); col++)
+			while(true)
 			{
-				String[] column = columns.get(col);
-						
-				for(int row = 0; row < column.length; row++)
-				{
-					placeStone(col, row, "");
-				}
+				int[] position = getUserPosition();
+				
+				int x = position[0];
+				int y = position[1] - 1;
+				
+				board.placeStone(x, y, "x");
+				board.draw();
 			}
-
-			drawBoard();
-			
-			int[] position = getUserPosition();
-			
-			placeStone(position[1]-1, position[0]-1, "x");
-			
-			drawBoard();
-			
-			System.out.println("POSITION x: " + position[0] + " y: " + position[1]);
 		}
 		else if(startORquit.equalsIgnoreCase("quit"))
 		{
 			System.out.println("...ENDING GAME...");
 		}
-	}
-	
-	private void placeStone(int x, int y, String tmpPlayerType)
-	{
-		columns.get(x)[y] = tmpPlayerType;
-	}
-	
-	private void drawBoard()
-	{
-		String[] yIndexes = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
-		
-		ColumnFormatter<String> ageFormatter = ColumnFormatter.text(Alignment.CENTER, 3);
-
-		Table.Builder builder = new Table.Builder("", yIndexes, ageFormatter);
-		
-		for(int i = 1; i <= 11; i++)
-			builder.addColumn(""+i, columns.get(i), ageFormatter);
-
-		Table table = builder.build();
-		System.out.println(table);
 	}
 	
 	private int[] getUserPosition()
@@ -96,7 +67,16 @@ class TmpDrawingClass
 		int x = position[0];
 		int y = position[1];
 		
-		String yesORno = promptString("You chose ["+ x + "," + y + "], correct (yes or no)?", new String[]{"yes", "no"}, "Try again and please enter yes or no.");
+		while(x > 11 || x < 1 || y > 11 || y < 1)
+		{
+			println("x and y must be between 1 and 11 inclusive. Please try again.");
+			
+			position = promptForPosition();
+			x = position[0];
+			y = position[1];
+		}
+		
+		String yesORno = promptString("You chose ["+ x + "," + y + "], correct? (yes or no)", new String[]{"yes", "no"}, "Try again and please enter yes or no.");
 		
 		while(yesORno.equalsIgnoreCase("no"))
 		{
@@ -106,7 +86,7 @@ class TmpDrawingClass
 			x = position[0];
 			y = position[1];
 			
-			yesORno = promptString("You chose ["+ x + "," + y + "], correct (yes or no)?", new String[]{"yes", "no"}, "Try again and please enter yes or no.");
+			yesORno = promptString("You chose ["+ x + "," + y + "], correct? (yes or no)", new String[]{"yes", "no"}, "Try again and please enter yes or no.");
 		}
 		
 		return position;
