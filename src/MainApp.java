c6aa0a8e557fdcc01779f2de477ee006b176eafd
Jephthia <jephthia4@hotmail.com>
2017-10-12 17:56:20 -0400

    Added user input

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,26 +1,65 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import io.bretty.console.table.Alignment;
+import io.bretty.console.table.ColumnFormatter;
+import io.bretty.console.table.Table;
 
 public class MainApp {
 
+	
 	public static void main(String[] args)
 	{
+
+		
+		
+		
+		
+		
+
+		
+
+
+		
 		new TmpDrawingClass();
 	}
+	
+
 }
 class TmpDrawingClass
 {
+	private List<String[]> columns = new ArrayList<String[]>();
 	private Scanner keyboard = new Scanner(System.in);
 	
 	public TmpDrawingClass()
-	{		
+	{
 		String startORquit = promptString("Type start to start the game and quit to quit the game", new String[]{"start", "quit"}, "Try again and please enter start or quit");
 		
 		if(startORquit.equalsIgnoreCase("start"))
 		{
 			System.out.println("...STARTING GAME...");
 			
+			for(int i = 0; i < 12; i++)
+				columns.add(new String[11]);
+			
+			for(int col = 0; col < columns.size(); col++)
+			{
+				String[] column = columns.get(col);
+						
+				for(int row = 0; row < column.length; row++)
+				{
+					placeStone(col, row, "");
+				}
+			}
+
+			drawBoard();
+			
 			int[] position = getUserPosition();
+			
+			placeStone(position[1]-1, position[0]-1, "x");
+			
+			drawBoard();
 			
 			System.out.println("POSITION x: " + position[0] + " y: " + position[1]);
 		}
@@ -28,6 +67,26 @@ class TmpDrawingClass
 		{
 			System.out.println("...ENDING GAME...");
 		}
+	}
+	
+	private void placeStone(int x, int y, String tmpPlayerType)
+	{
+		columns.get(x)[y] = tmpPlayerType;
+	}
+	
+	private void drawBoard()
+	{
+		String[] yIndexes = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
+		
+		ColumnFormatter<String> ageFormatter = ColumnFormatter.text(Alignment.CENTER, 3);
+
+		Table.Builder builder = new Table.Builder("", yIndexes, ageFormatter);
+		
+		for(int i = 1; i <= 11; i++)
+			builder.addColumn(""+i, columns.get(i), ageFormatter);
+
+		Table table = builder.build();
+		System.out.println(table);
 	}
 	
 	private int[] getUserPosition()
