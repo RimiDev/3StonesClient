471bc6b84cc4f3b0c9aa376aeef9cda18e180266
Maxime Lacasse <maxime.lacasse@dawsoncollege.qc.ca>
2017-10-25 10:45:42 -0400

    Server works

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,7 +1,60 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+
+
 public class MainApp
 {	
+	
+	static boolean isValid = false;
+	
 	public static void main(String[] args)
-	{	
-		AppGUI gui = new AppGUI();
+	{					
+		//Scanner object
+		Scanner clientInput = new Scanner(System.in);
+		
+		String serverAddress;
+		String port = "";
+		
+		System.out.println("Please enter an IP address: ");
+		serverAddress = clientInput.nextLine();
+		
+		while (!isValid) {
+			System.out.println("Please enter a PORT number: ");
+			port = clientInput.nextLine();
+			isValid = inputValidation(port);
+		} 
+		
+		clientInput.close();
+		
+		
+		//Establishing a connection.
+		try (Socket clientSocket = new Socket(serverAddress, Integer.valueOf(port))){
+			
+			AppGUI gui = new AppGUI();
+			
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		
+		
+	} // end of main
+	
+	
+	//Validation for the port number, to check if it is a numeric input.
+	public static boolean inputValidation(String port) {
+		
+		  return port.matches("[0-9]+");
+		
 	}
-}
+	
+	
+	
+} // end of class
